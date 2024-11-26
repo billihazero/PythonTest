8eb40a32a6698a5da63e41060cecbdc7d623e7a4
billihazero <billihazero@gmail.com>
2024-11-26 15:44:39 +0900

    문자열돌리기

---
+++ 기초 트레이닝/문자열 붙여서 출력하기.py
@@ -0,0 +1,8 @@
+str1, str2 = input().strip().split(' ')
+
+# if len(str1) and len(str2) <1 and len(str1) and len(str2) > 10:
+#     print("10보다 작게 작성하쇼")
+# else :
+#     print(str1+str2)
+
+print(input().strip().replace(' ', ''))
